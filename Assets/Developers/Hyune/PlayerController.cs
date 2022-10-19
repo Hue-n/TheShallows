866ac0adaa026018230ac01cd9688090e43004ce
Hue-n <72900340+Hue-n@users.noreply.github.com>
2022-10-18 21:54:56 -0400

    Player Controller Input Implementation
    Player Controller Stuff

--- Assets/Developers/Hyune/PlayerController.cs
+++ Assets/Developers/Hyune/PlayerController.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public DefaultControls controls;
+
     public float maxSpeed;
     public float maxTurn;
 
@@ -19,6 +22,32 @@ public class PlayerController : MonoBehaviour
     private Vector3 moveDir;
     private Vector3 input;
 
+    private void OnEnable()
+    {
+        controls.Controller.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Controller.Disable();
+    }
+
+    private void Awake()
+    {
+        controls = new DefaultControls();
+
+        controls.Controller.Movement.performed += ctx => DebugLog();
+        controls.Controller.Movement.performed += ctx => input = new Vector3(ctx.ReadValue<Vector2>().x, 0,
+            ctx.ReadValue<Vector2>().y);
+    }
+
+    private void OnDestroy()
+    {
+        controls.Controller.Movement.performed -= ctx => DebugLog();
+        controls.Controller.Movement.performed -= ctx => input = new Vector3(ctx.ReadValue<Vector2>().x, 0,
+            ctx.ReadValue<Vector2>().y);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -28,7 +57,7 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        //input = new Vector3(gamepad.leftStick.ReadValue().x, 0, gamepad.leftStick.ReadValue().y);
 
         currentTurn += input.x * turnScalar;
         currentTurn = Mathf.Clamp(currentTurn, -maxTurn, maxTurn);
@@ -37,6 +66,11 @@ public class PlayerController : MonoBehaviour
         currentSpeed = Mathf.Clamp(currentSpeed, -maxSpeed, maxSpeed);
 
         moveDir = transform.forward * currentSpeed;
+    }
+
+    public void DebugLog()
+    {
+        Debug.Log(input);
     }
 
     // Update is called once per frame
