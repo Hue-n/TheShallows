8b9ad2ddf64b83d205798a0011a72bfb40029fe2
HarrisonDresser <harry.dresser777@gmail.com>
2022-10-26 23:25:31 -0400

    Pause Menu established
    still needs some options and touching up the buttons

---
+++ Assets/Developers/Harry/PauseMenu.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    public Animator animator;
+
+    public bool SettingsIsOpen = false;
+
+    public bool PauseIsOpen = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+              
+                animator.SetBool("CloseAll", true);
+                Resume();
+
+                if (SettingsIsOpen == true)
+                {
+                    animator.SetBool("SettingsIsOpen", false);
+                    animator.SetBool("CloseAll", true);
+                }
+                
+            }
+            else
+            {
+                
+                Pause();
+
+            }
+        }
+    }
+
+    public void Resume()
+    {
+ 
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        animator.SetBool("PauseIsOpen", false);
+
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        animator.SetBool("PauseIsOpen", true);
+    }
+
+    public void QuitGame()
+    {
+
+    }
+
+    public void LoadSettings()
+    {
+        animator.SetBool("SettingsIsOpen", true);
+        animator.SetBool("PauseIsOpen", false);
+        SettingsIsOpen = true;
+
+    }
+
+    public void CloseSettings()
+    {
+        animator.SetBool("SettingsIsOpen", false);
+        animator.SetBool("PauseIsOpen", true);
+        animator.SetBool("CloseAll", false);
+        SettingsIsOpen = false;
+
+    }
+
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(3);
+    }
+
+}
