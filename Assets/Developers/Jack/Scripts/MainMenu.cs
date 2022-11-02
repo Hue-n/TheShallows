15b95afa7b459d7f27c8d18aba533b60c9f67419
BuriedDonut <Jared.toavs@knights.ucf.edu>
2022-11-02 15:26:20 -0400

    Main Menu is now spruced up

--- Assets/Developers/Jack/Scripts/MainMenu.cs
+++ Assets/Developers/Jack/Scripts/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
     public GameObject menu;
     public GameObject settings;
     public GameObject credits;
+    public GameObject controls;
     public AudioClip TitleMusic;
     
     public void Start()
@@ -19,11 +20,28 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("WaveSurvival");
     }
 
+    public void controlsMenu()
+    {
+        settings.SetActive(false);
+        credits.SetActive(false);
+        controls.SetActive(true);
+        menu.SetActive(false);
+    }
+    
+    public void creditsMenu()
+    {
+        settings.SetActive(false);
+        credits.SetActive(true);
+        controls.SetActive(false);
+        menu.SetActive(false);
+    }
+
     public void ReturnToMenu()
     {
         settings.SetActive(false);
         credits.SetActive(false);
         menu.SetActive(true);
+        controls.SetActive(false);
     }
 
     public void QuitGame()
